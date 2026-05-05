f2afec11a33d923287b3925c2e00883987267030
gangodawilae <gangodawilae@wsc.school.nz>
2026-05-05 14:54:58 +1200

    Made some basic coding and made emojis using python

---
+++ Lesson 1- output and input/ascii_art.py
@@ -0,0 +1,25 @@
+### ASCII Art ###
+# Create a picture using lines, spaces and/or some letters.
+# You'll need to use print multiple times to create your picture.
+print(" __________")
+print("/          \ ")
+print("|   O   O   |")
+print("|     V     |")
+print("\          /")
+print(" __________")
+
+print("___________")
+print("/          \ ")
+print("|  o    o   |")
+print("|     M     |")
+print("\           /")
+print("_____________")
+
+      
+
+
+
+# -------------------------
+
+# EXTENSION
+# Try to create a more complicated ASCII picture
---
+++ _main.py
@@ -0,0 +1,5 @@
+print("hello")
+print()
+print("hello")
+print("I'm coding")
+print("I'm coding")

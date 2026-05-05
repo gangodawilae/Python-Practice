34de1adb9f549661349794a71403aa8334d40078
gangodawilae <gangodawilae@wsc.school.nz>
2026-05-05 15:06:04 +1200

    Added inputs

--- _main.py
+++ _main.py
@@ -3,3 +3,16 @@ print()
 print("hello")
 print("I'm coding")
 print("I'm coding")
+
+input("Name?")
+print("Hello")
+
+input("Name?\n")
+print("Name?")
+input()
+
+input("School?\n")
+print("School")
+input()
+
+

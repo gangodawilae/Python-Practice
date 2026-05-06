75a1a7b568e936fed63ded7ba4033a547483d608
gangodawilae <gangodawilae@wsc.school.nz>
2026-05-07 10:28:26 +1200

    Made madlib story but didn't finish it

---
+++ Lesson 2- Data types and variables/madlib.py
@@ -0,0 +1,33 @@
+# Create a short Madlib: Get input from your user (a bunch of words), 
+# then output a madlib using those words.
+
+# Ask user for a name and save it in a variable
+name=input("\nplease enter your name")
+# adjective= input ("Enter an adjective (describing the name):")
+# place=input("Enter a place:")
+
+# Ask user for an animal and save it in a variable
+animal=input("please enter a animal name")
+
+# Ask user for a colour and save it in a variable
+color=input("please enter a color")
+
+
+# Ask user for an object and save it in a variable
+object=input("please enter a object name")
+
+# Print your madlib using the 4 variables above.
+print("There was a" + name )
+print("She was" + color)
+# ----------------------------
+
+# EXTENSION
+# Research about 'print formatting in python'. 
+# Use what you learn to rewrite your madlib into easier to read code.
+
+# ----------------------------
+
+# EXPERT (for those who already know some Python)
+# Create a randomised madlib game
+# GOAL: Just like above except...
+#       Write 4-6 different madlibs and randomise which one is output.
--- _main.py
+++ _main.py
@@ -1,24 +1,39 @@
-# print("hello")
-# print()
-# print("hello")
-# print("I'm coding")
-# print("I'm coding")
+# # print("hello")
+# # print()
+# # print("hello")
+# # print("I'm coding")
+# # print("I'm coding")
 
-# input("Name?")
-# print("Hello")
+# # input("Name?")
+# # print("Hello")
 
-# input("Name?\n")
-# print("Name?")
-# input()
+# # input("Name?\n")
+# # print("Name?")
+# # input()
 
-# input("School?\n")
-# print("School")
-# input()
+# # input("School?\n")
+# # print("School")
+# # input()
 
-print(5-5)
-print("5" + "5")
-print(5.0 / 5.0)
-#print("5" + 5)
-print(5+ 5.0)
+# print(5-5)
+# print("5" + "5")
+# print(5.0 / 5.0)
+# print("5" + 5)
+# print(5+ 5.0)
 
-print(2 ** 3)
+name = "Helen"
+print("My name is" + name)
+
+name = "Helen"
+print("My name is", name)
+
+# name="Helen"
+# age = 15
+# print(name + " is " + age)
+
+name = "Helen"
+age = 15
+print(name, "is", age)
+
+
+

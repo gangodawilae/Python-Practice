1c94d70e3fed9cc1b963f948d2cf977b70c03515
gangodawilae <gangodawilae@wsc.school.nz>
2026-05-11 19:28:39 +1200

    Did the debugging in lesson 3

--- Lesson 3- Comments/debugging_3.py
+++ Lesson 3- Comments/debugging_3.py
@@ -1,16 +1,16 @@
 # 3 Question Quiz - Debug all the errors, including any semantic errors.
 
 print("Hello!")
-print("This is a quick 3 part quiz. A question will be asked, and then you answer it")
+INSTRUCTIONS="This is a quick 3 part quiz. A question will be asked, and then you answer it"
 
-print("First question:")
+ques1=print("First question:")
 ans1=input("How many millimetres in a centimetre?")
 print("The answer is 10!")
 
-print("Next Question:")
+ques2=print("Next Question:")
 ans2=input("What is the capital of New Zealand?")
 print("The answer is Wellington. That was easy, wasn't it?")
 
-print("Final Question!")
+ques3=print("Final Question!")
 ans3=input("What is 3 x 6?")
 print("The answer is 18!")

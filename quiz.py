bcb8f50360d9b5a764da785f83404ee46c15cd47
gangodawilae <gangodawilae@wsc.school.nz>
2026-05-12 15:07:29 +1200

    Craeted the quiz but there's some changes to do

--- quiz.py
+++ quiz.py
@@ -1,8 +1,10 @@
 #Starts with 0
-score = 0
+score =0
 print("Hello!")
 print("Welcome to my quiz game!")
 print("Choose the correct option,(a, b, or c)")
+input("Are you ready...?")
+print("Let's start!!, I believe in you")
 
 #Question 1 with answers
 print("1. who is the first person to space?")
@@ -32,8 +34,8 @@ if answer == "a":
    score= score + 1
 
 #Question 4 with answers
-print("4. What is Mount Olympus located")
-print("a)Athens")
+print("4. Where is Mount Olympus located")
+print("a)Rome")
 print("b)Russia")
 print("c)Greece")
 answer=input("Your answer: ")
@@ -41,7 +43,7 @@ if answer == "c":
    score= score + 1
 
 #Question 5 with answers
-print("5. What does cows drink")
+print("5. What does cows drink?")
 print("a)Water")
 print("b)Milk")
 print("c)Tea")
@@ -59,11 +61,42 @@ if answer == "c":
    score= score + 1
 
 #Question 7 with answers
-print("7.What is the diffrence between object mode and edit mode?")
+print("7.What is the diffrence between object mode and edit mode in blender?")
 print("a)Object mode is for animating and edit mode is for texturing")
 print("b)Object mode is for modelling and edit mode is for moving objects around ")
 print("c) Modelling like positioning,rotating adding objects and edit mode is modifying of a object's internal geomatry like moving, and deleting vertices,edges,and faces and etc. ")
 answer=input("Your answer: ")
 if answer == "c":
    score= score + 1
+
+#Question 8 with answers
+print("8.Which one is the largest planet?")
+print("a)Saturn")
+print("b)Jupiter")
+print("c)Sun")
+answer=input("Your answer: ")
+if answer == "b":
+   score= score + 1
+
+#Question 9 with answers
+print("9. What is the largest organ in the human body?")
+print("a)Skin")
+print("b)Heart")
+print("c)Lungs")
+answer=input("Your answer: ")
+if answer == "a":
+   score= score + 1
+
+#Question 10 with answers
+print("10.What is the smallest unit of matter")
+print("a)Molecule")
+print("b)Atom")
+print("c)Proton")
+answer=input("Your answer: ")
+if answer == "b":
+   score= score + 1
+
 print("\nYou got", score, "correct out of 10")
+#Conclution part
+print("Yay! I sure that you got most of them correct, may be all")
+print("Thanks for playing my game")

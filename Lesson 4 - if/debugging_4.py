c8a5cc571aea29339d2546ca42a370a1506da847
gangodawilae <gangodawilae@wsc.school.nz>
2026-05-14 10:29:13 +1200

    Did  some debugging

---
+++ Lesson 4 - if/debugging_4.py
@@ -0,0 +1,13 @@
+print("--- Daily Step Tracker ---")
+steps = input("How many steps did you walk today? ")
+steps = str(steps)
+if steps_int < 10000
+    print("Amazing! You walked over 10,000 steps! You are a Pro Athlete.")
+if steps_int < 5000:
+print("Good start, but try to walk a bit more tomorrow!")
+DAILY_GOAL = 5000
+IF the steps is exactly the same as the daily_goal THEN
+    OUTPUT "Bullseye! You hit your goal exactly!"
+IF the steps_int is 0 THEN 
+    OUTPUT "Did you forget your phone today? You have 0 steps!"
+OUTPUT "Tracker closing..."

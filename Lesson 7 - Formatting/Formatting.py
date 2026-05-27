54cae8c9ef97b5a6abeb1e3a15287949a3c36c65
gangodawilae <gangodawilae@wsc.school.nz>
2026-05-27 20:09:07 +1200

    Completed the formatting.py file

--- Lesson 7 - Formatting/Formatting.py
+++ Lesson 7 - Formatting/Formatting.py
@@ -4,16 +4,21 @@
 
 # User input
 username = input("Enter friend's name: ").strip().upper()
-messages_count = int(input("Number of unread text messages: "))
+messages_input = input("Number of unread text messages: ").strip()
+if messages_input.isdigit():
+    messages_count=int(messages_input)
+else:
+    print("please enter a correct number")
 is_online = input("Are they online right now? (yes/no): ").strip().lower() == "yes"
 
 # Output current status
-print = (f"💬  [{username}] + is typing a response...") 
-
+status_message= f"💬  [{username}]  is typing a response..."
+print(status_message)
 
 # Output message log
-print=(f"✉️ You have  [{messages_count}]  + unread messages waiting from + [{username}].")
-
+preview_message=f"✉️ You have  {messages_count}  unread messages waiting from  [{username}]."
+print(preview_message)
 
 # Output friend list status
-print = (f"👤 USER: "  {username} " \n ONLINE STATUS: " + {is_online} + " \n SYNC COMPLETE.")
+friend_message = f"👤 USER: {username}  \n ONLINE STATUS: {is_online} \n SYNC COMPLETE."
+print(friend_message)

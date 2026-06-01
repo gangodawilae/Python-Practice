2c5e7acbd23fa09086df44eca7beeda4915e8598
gangodawilae <gangodawilae@wsc.school.nz>
2026-06-01 12:17:51 +1200

    Commented the quiz properly

--- quiz.py
+++ quiz.py
@@ -4,12 +4,12 @@ TOTAL_QUESTIONS = 11
 #Starts with 0 points
 score =0
 
-#Ask for users's name
+#Ask for users's name, remove extra space and write it nicely
 user_name=input("Hello! What is your name?").strip().title()
 
-#Start of the quiz with a welcome to my quiz
+#Start of the quiz with a welcome message to my quiz
 print(f"Welcome to my quiz game! {user_name}")
-print("Choose the correct option,(a, b, or c)")
+print("INSTRUCTIONS:Choose the correct option,(a, b, or c) or write the answer")
 input("Are you ready...? Press yes to start")
 print(f"\nLet's start!!, I believe in you, {user_name}!")
 
@@ -20,6 +20,7 @@ print("a) Yuri Gagarin")
 print("b)Thomas Eddison")
 print("c)Neil Armstrong")
 
+#Get answer in lowercase with proper spacing
 answer=input("Your answer: ").strip().lower()
 
 #Condiional code using if and elif and else
@@ -33,10 +34,10 @@ else:
 
 
 #question 2
-print("\n. What is 5+3?")
+print("\n2. What is 5+3?")
 answer= input("Your answer:").strip()
 
-#Turning string into a integer
+#Turning string into a integer before comparing
 if int(answer) ==8:
    print(f"Correct, {user_name}. Your are great at maths!")
    score=score +1
@@ -44,7 +45,7 @@ else:
    print(f"Wrong,{user_name}. The answer is 8")
 
 
-#Question 3 with answers
+#Question 3 
 print("\n3. Which bird can fly backward?")
 print("a) Blue duck")
 print("b)Hummingbird")
@@ -62,7 +63,7 @@ else:
    print(f"Wrong,{user_name}.The answer is b")
 
 
-#Question 4 with answers
+#Question 4 
 print("\n4. What is the nation bird of New Zealand?")
 print("a)kiwi")
 print("b)Tui")
@@ -78,11 +79,12 @@ else:
    print(f"Wrong,{user_name}. The answer is a")
 
 
-#Question 5 with answers
-print("5. Where is Mount Olympus located")
+#Question 5 
+print("\n5. Where is Mount Olympus located")
 print("a)Rome")
 print("b)Russia")
 print("c)Greece")
+
 answer=input("Your answer: ").strip().lower()
 
 if answer == "c" or answer=="greece":
@@ -94,26 +96,31 @@ else:
    print(f"Wrong,{user_name}. The answer is c.")
 
 
-#Question 6 with answers
-print("6. What does adult cows drink?")
+#Question 6 
+print("\n6. What does adult cows drink?")
 print("a)Water")
 print("b)Milk")
 print("c)Tea")
+
 answer=input("Your answer: ").strip().lower()
+
 if answer == "a" or answer == "water":
    print(f"Correct!, {user_name}")
    score= score + 1
 elif answer == "b" or answer == "milk":
    print(f"Tricky question, {user_name}. Baby cows drink milk, not adult cows ")
 else:
-   print("You must choose a,b or c. Try again" )
+   print(f"Wrong, {user_name}. The answer is a" )
 
-#Question 7 with answers
-print("7. What is 128+22-5")
+
+#Question 7 
+print("\n7. What is 128+22-5")
 print("a)117")
 print("b)35")
 print("c)145")
+
 answer=input("Your answer: ").strip().lower()
+
 if answer == "c" or answer== "145":
    print(f"correct!, {user_name}. Good Job. ")
    print(" (.^.)")
@@ -123,12 +130,14 @@ elif answer == "a" or answer == "117":
 else:
    print(f"Wrong, {user_name} The answer is c.")
 
-#Question 8 with answers
+#Question 8 
 print("\n8.What is the diffrence between object mode and edit mode in blender?")
 print("a)Object mode is for animating and edit mode is for texturing")
 print("b)Object mode is for modelling and edit mode is for moving objects around ")
 print("c)Object mode is for position, rotate and scale the objects while edit mode is for change their geoetry. ")
+
 answer=input("Your answer: ").strip().lower()
+
 if answer == "c" or answer == "geometry" in answer:
    print(f"Correct!, {user_name}")
    score= score + 1
@@ -137,41 +146,46 @@ elif answer == "b":
 else:
    print(f"Wrong, {user_name}.The correct answer is c")
 
-#Question 9 with answers
+#Question 9 
 print("\n9.Which one is the largest planet?")
 print("a)Saturn")
 print("b)Jupiter")
 print("c)Sun")
+
 answer=input("Your answer: ").strip().lower()
+
 if answer == "b" or answer == "jupiter":
    print(f"Correct!, {user_name}.")
    score= score + 1
 elif answer == "a":
-   print(f"Saturn is ne of the big planet, but not the biggest, {user_name}")
+   print(f"Saturn is one of the biggest planet, but not the biggest, {user_name}")
 else:
    print(f"Wrong, {user_name}. The correct answer is b")
 
-#Question 10 with answers
-print("10. What is the largest organ in the human body?")
+#Question 10 
+print("\n10. What is the largest organ in the human body?")
 print("a)Skin")
 print("b)Heart")
 print("c)Lungs")
+
 answer=input("Your answer: ").strip().lower()
+
 if answer == "a" or answer == "skin":
-   print('Correct!')
-   print(r"\^o^/")
+   print('Correct! \\^o^/')
    score= score + 1
 elif answer == "b":
    print(f"Heart is important, but not the largest orgen,{user_name}")
 else:
    print(f"Wrong,{user_name}. The answer is a")
 
-#Question 11 with answers
-print("11.What is the smallest unit of matter")
+#Question 11 
+print("\n11.What is the smallest unit of matter")
 print("a)Molecule")
 print("b)Atom")
 print("c)Proton")
+
 answer=input("Your answer: ").strip().lower()
+
 if answer == "b" or answer== "atom":
    print(f"Correct!,{user_name}  (o_^)")
    score= score + 1
@@ -180,10 +194,10 @@ elif answer == "a":
 else:
    print(f"Wrong,{user_name}. The answer is b.")
 
-# Final Scoring
+# Final Scoring and feedback
 print(f"\n{user_name}, you got, {score} correct out of {TOTAL_QUESTIONS}!")
 
-#Logical operator
+#Give feedback based on the score
 if score>=8 and score <=11:
    print("Excellent work! You are amazing.")
 elif score>= 5 and score<8 :

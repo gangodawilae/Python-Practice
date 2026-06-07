901397aa69503190cf541fb428543d51347a6211
gangodawilae <gangodawilae@wsc.school.nz>
2026-06-08 11:56:55 +1200

    Started to do the safe_cracker file, but didn't finish

--- Esandi_main.py
+++ Esandi_main.py
@@ -149,9 +149,33 @@
 # while user_input != "exit":
 #     user_input = input()
 
-user_input = 'idk'
-try:
-    num = int(user_input)
-    print(f'You picked {num}')
-except:
-	print(f'{user_input} is not a number!')
+# user_input = 'idk'
+# try:
+#     num = int(user_input)
+#     print(f'You picked {num}')
+# except:
+# 	print(f'{user_input} is not a number!')
+
+# count = 0
+# while count < 10:
+#     print('Counting…')
+#     if count < 5:
+# 	    break
+#     print('Almost done…')
+# print('Finished!')
+
+
+# count = 0
+# while count < 10:
+#     print('Counting…')
+#     if count < 5:
+# 	    continue
+#     print('Almost done…')
+# print('Finished!')
+
+while True:
+	guess = input('Guess a country')
+if guess.lower().strip() == 'malawi':
+	break
+print('Try again.')
+print('Well done!')

d2f45a23d54aa6e11af848c43da41975966a790f
gangodawilae <gangodawilae@wsc.school.nz>
2026-06-15 11:58:47 +1200

    Did the debugging in file 11

---
+++ Lesson 11 - For/debugging.py
@@ -0,0 +1,37 @@
+
+banned_items = ["slingshot","laser"]
+#Create a list of invetory items
+inventory = ["apple","slingshot","book","laser"]
+confiscated=[]
+
+print(f"Scanning inventory: (inventory)")
+
+i=0 
+while i<len(inventory):
+    item=inventory[i]
+    if item in banned_items:
+        print(f"Alert! Found banned item: {inventory[0]}")
+        confiscated.append(item)
+        inventory.pop(i)
+    else:
+        i+=1
+
+print(f"Scan complete. Total flag matches: {len(banned_items)}")
+
+if len(confiscated)>0:
+    print("Items confiscated.")
+
+    #For number of items that confiscated in the list
+    for i in range(len(confiscated)):
+       print(f"{i + 1}. {confiscated[i].capitalize()}")
+
+print("Remaining intenvory:{inventory}")
+       
+        # if item in banned_items:
+           
+
+
+
+
+    # FOR the number of items in the confiscated list (use index)
+        # PRINT the item listed with a number (eg. 1. Laser)

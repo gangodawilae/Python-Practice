e8c97856a06d6a489f80f8dcf6add8b3c4534b47
gangodawilae <gangodawilae@wsc.school.nz>
2026-06-21 03:09:12 +1200

    I couldn't save this file, debuggin in lesson 2, so now I save it

--- Lesson 2- Data types and variables/debugging_2.py
+++ Lesson 2- Data types and variables/debugging_2.py
@@ -1,16 +1,19 @@
 # 3 Question Quiz - Debug all the errors, including any semantic errors.
-
+#introduction
 print("Hello!")
 print("This is a quick 3 part quiz. A question will be asked, and then you answer it")
 
+#First question with answer
 print("First question:")
 ans1=input("How many millimetres in a centimetre?")
 print("The answer is 10!")
 
+#Second question with answer
 print("Next Question:")
 ans2=input("What is the capital of New Zealand?")
 print("The answer is Wellington. That was easy, wasn't it?")
 
+#Third question with answer
 print("Final Question!")
 ans3=input("What is 3 x 6?")
 print("The answer is 18!")

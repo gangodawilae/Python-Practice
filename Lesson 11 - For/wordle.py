46cbbf80cfde636a591c8b144eac5d60c234379a
gangodawilae <gangodawilae@wsc.school.nz>
2026-06-21 03:07:55 +1200

    Finished the function file, menu file and I added more features to the wordle file that I did ealier like Iadded, if the user put a guess and if any of the letters in the guess is equal to the correct word, those words should be show in red, yellow, and green.

--- Lesson 11 - For/wordle.py
+++ Lesson 11 - For/wordle.py
@@ -2,91 +2,139 @@
 # PROJECT: Wordle
 # Create a program where the user must guess the 5 letter word.
 # =====================================================================
-
-# TOOLS
-# Import random so you can randomise the word
 import random
-
-# VALUES
-#  Create a list of at least 5 different 5-letter words
-words= ["apple", "chair", "smile", "bread"]
-
-play=True
-
-# INTRODUCTION
-# Tell your user how to play wordle (make sure they know they must input 5 letter words)
-print("Welcome to the wordle.")
-print("Guess the 5 letter word. You must enter 5 letter guesses only!")
+#===============================
 
 
-#Create a main loop
-while play:
+#===============================
+# FUNCTIONS
+#===============================
 
-    # pick a random word
-    word= random.choice(words)
+# Run program 1:Wordle
+words = ["apple","chair","smile","bread"]
+secret = random.choice(words)
 
-    # USER INPUT
-    #  Get user's first guess and save it into a variable
-    guess= input("Enter your 5 letter guess: ").lower()
+print("\n Welcome to the..... Wordle!!")
+print("Guess the 5 letter word. You must enter 5 letter guesses only.\n Type 'quit' to exit.")
 
-    # Make sure that it is in 5 letters using len and tell them that it's not 5 letters
-    while len(guess) !=5:
-        print("That's not 5 lettters. Try again!")
-        guess=input("Enter a 5 letter word").lower()
-        
+while True:
+    guess= input("Enter your guess: ").lower()
 
-    # Check if they got it correct and if they did, tell them so and then break the loop
-    if guess == word:
-        print("Correct! You guessed the word right!.\n Good Job.")
-        again = input("Play again (yes/no): ").lower()
-        if again != "yes":
-            play = False
+
+    if guess == "quit":
+        print("Goodbye")
+        break
+
+    if len(guess) != 5:
+        print("Guess must be 5 letters!")
         continue
 
-    
-    # # Letter by letter checking
-    # for i in range(5):
+    if guess == secret:
+        print("\033[32mCorrect! You guessed the word!\033[0m")
+        break
 
-    #     # Check if the current letter is in the correct position
-    #     if guess[i]== word [i]:
-    #         print(guess[i], "is correct and in the right place!")
-
-    #     # Otherwise check if the letter is somewhere else in the word.
-    #     elif guess[i] in word:
-    #         print(guess[i], "is in the word but in the wrong place")
-
-    #     # Else tell them that letter is wrong
-    #     else:
-    #         print(guess[i], "is not in the word")
-
-
-# ==========================================================
-# EXTENSION
-
-# Instead of telling the user one by one about their letters, put each correct letter and _ for a wrong letter into a list. 
-    result = []
-
-    #A for loop that loops 5 times
+    result = ""
     for i in range(5):
-        #Check if the current letter of user_input (guess[i]) is the same as i letter of the word
-        if guess[i] == word[i]:
-            result.append(guess[i])
+        if guess[i] == secret[i]:
+            result += f"\033[32m{guess[i]}\033[0m"
 
+        elif guess[i] in secret:
+            result += f"\033[33m{guess[i]}\033[0m"
 
-        elif guess[i] in word:
-            result.append(guess[i])
-
-        #If not tell the user that the letter is wrong
         else:
-            result.append("_")
+            result += f"\033[31m{guess[i]}\033[0m"
+
+        print("Result:", result)
+
+print("The word was:", secret )
 
 
-    print("Result:", "".join(result))
 
-    # Ask if they want to play again. If they don't, set play to false.
-    again = input("Play again?\n yes \n no: ").lower()
-    if again != "yes":
-        play= False
+
+# # TOOLS
+# # Import random so you can randomise the word
+# import random
+
+# # VALUES
+# #  Create a list of at least 5 different 5-letter words
+# words= ["apple", "chair", "smile", "bread"]
+
+# play=True
+
+# # INTRODUCTION
+# # Tell your user how to play wordle (make sure they know they must input 5 letter words)
+# print("Welcome to the wordle.")
+# print("Guess the 5 letter word. You must enter 5 letter guesses only!")
+
+
+# #Create a main loop
+# while play:
+
+#     # pick a random word
+#     word= random.choice(words)
+
+#     # USER INPUT
+#     #  Get user's first guess and save it into a variable
+#     guess= input("Enter your 5 letter guess: ").lower()
+
+#     # Make sure that it is in 5 letters using len and tell them that it's not 5 letters
+#     while len(guess) !=5:
+#         print("That's not 5 lettters. Try again!")
+#         guess=input("Enter a 5 letter word").lower()
+        
+
+#     # Check if they got it correct and if they did, tell them so and then break the loop
+#     if guess == word:
+#         print("Correct! You guessed the word right!.\n Good Job.")
+#         again = input("Play again (yes/no): ").lower()
+#         if again != "yes":
+#             play = False
+#         continue
+
+    
+#     # # Letter by letter checking
+#     # for i in range(5):
+
+#     #     # Check if the current letter is in the correct position
+#     #     if guess[i]== word [i]:
+#     #         print(guess[i], "is correct and in the right place!")
+
+#     #     # Otherwise check if the letter is somewhere else in the word.
+#     #     elif guess[i] in word:
+#     #         print(guess[i], "is in the word but in the wrong place")
+
+#     #     # Else tell them that letter is wrong
+#     #     else:
+#     #         print(guess[i], "is not in the word")
+
+
+# # ==========================================================
+# # EXTENSION
+
+# # Instead of telling the user one by one about their letters, put each correct letter and _ for a wrong letter into a list. 
+#     result = []
+
+#     #A for loop that loops 5 times
+#     for i in range(5):
+#         #Check if the current letter of user_input (guess[i]) is the same as i letter of the word
+#         if guess[i] == word[i]:
+#             result.append(guess[i])
+
+
+#         elif guess[i] in word:
+#             result.append(guess[i])
+
+#         #If not tell the user that the letter is wrong
+#         else:
+#             result.append("_")
+
+
+#     print("Result:", "".join(result))
+
+#     # Ask if they want to play again. If they don't, set play to false.
+#     again = input("Play again?\n yes \n no: ").lower()
+#     if again != "yes":
+#         play= False
     
 # ==========================================================
 # EXPERT
---
+++ Lesson 13 - Functions/menu_normal.py
@@ -0,0 +1,218 @@
+"""
+PROGRAM: Menu
+This starts with a menu so users can run 1 of 3 different programs:
+1.
+2.
+3.
+"""
+
+# INSTRUCTIONS
+# TODO Copy over the code from 3 of your other programs into their own function.
+# TODO If you have any imports, make sure to move them out of the function to IMPORTS section 
+# (everything else can stay in the functions)
+# TODO Create a menu program in the main function and call each program function based on user input
+
+#===============================
+# IMPORTS
+import random
+#===============================
+
+
+#===============================
+# FUNCTIONS
+#===============================
+
+# Run program 1:Wordle
+def run_wordle():
+    words = ["apple","chair","smile","bread"]
+    secret = random.choice(words)
+
+    print("\n Welcome to the..... Wordle!!")
+    print("Guess the 5 letter word. You must enter 5 letter guesses only.\n Type 'quit' to exit.")
+
+    while True:
+        guess= input("Enter your guess: ").lower()
+
+
+        if guess == "quit":
+            print("Goodbye")
+            break
+
+        if len(guess) != 5:
+            print("Guess must be 5 letters!")
+            continue
+
+        if guess == secret:
+            print("\033[32mCorrect! You guessed the word!\033[0m")
+            break
+
+        result = ""
+        for i in range(5):
+            if guess[i] == secret[i]:
+                result += f"\033[32m{guess[i]}\033[0m"
+
+            elif guess[i] in secret:
+                result += f"\033[33m{guess[i]}\033[0m]"
+
+            else:
+                result += f"\033[31m{guess[i]}\033[0m]"
+
+        print("Result", result)
+
+    print("The word has:", secret )
+
+# Run program 2: Magic 8 Ball
+
+def run_magic_8ball():
+    print("Welcome to...... \n Magic-8 ball!!")
+
+# TODO: Create a list called 'responses' that contains at least 8 different responses.
+    common_responses = ["Yes, definetly",
+             "Absolutly",
+             "Yeah, probably",
+            "I'm not sure",
+             "Could be",
+             "No, definetly not",
+             "Maybe... but try again later",
+             "I guess soo...",
+             "Not really sure yet"]
+
+    rare_responses = ["I don't think so...",
+                  "Honestly, I have no idea",
+                  "Ummm. I don't think I should answer that question",
+                  "I'm not sure about that one",
+                  "Ask me that again another day",
+                  "Hmmm. That's a tough one to answer actually"]
+
+# MAIN LOOP
+# TODO Create an infinite loop
+    while True:
+        question=input("Ask a yes/no question about your future (or type 'quit' to leave): ")
+    
+    # Check if the user wants to exit and break from the loop if they do.
+        if question.lower() == "quit":
+            print("Good bye!!")
+            break
+
+    
+        chance = random.random()
+
+    #Check if the number is lower than 0.8 and use the common list
+        if chance <0.8:
+            chosen_fortune = random.choice(common_responses)
+
+    
+    #Otherwise use the rare list
+        else:
+            chosen_fortune = random.choice(rare_responses)
+
+        #Print the result.
+        print("Magic 8 ball says..:", chosen_fortune )
+
+
+
+# Run program 3: Pokemon battle
+# =====================================================================
+# PROJECT: Pokemon
+# Create a battle program where you battle a random pokemon
+# =====================================================================
+
+def run_pokemon():
+    print("Welcome to......\n Pokemon Battle!!")
+
+# Wild Pokemon
+# Create a multidimensional list that holds 4 pokemon names and their max health (you choose)
+    wild_pokemon = [
+        {"name":"pikachu","health": 60},
+        {"name":"Charmander","health":55},
+        {"name":"Squirtle","health":65},
+        {"name":"Bulbasaur","health": 70}
+    ]
+# User Pokemon
+# Create a multidimensional list that holds 4 pokemon attacks and their different damage
+    user_attacks=[
+        {"move":"Tackle","damage":10},
+        {"move":"Thunderbolt","damage":15},
+        {"move":"Quick Attack","damage":12},
+        {"move":"Mega punch", "damage":20}
+    ]
+
+#  Create a variable to hold a randomised wild pokemon
+    enemy= random.choice(wild_pokemon)
+    current_health=enemy["health"]
+
+    print(f"A wild {enemy['name']} appeared! It has the {current_health} health points. ")
+
+#Battle loop
+    while current_health>0:
+
+    #Show attack options
+        print("\nChoose your attack:")
+        for i in range(len(user_attacks)):
+            print(f"{i+1}. {user_attacks[i]['move']} ({user_attacks[i]['damage']} damage)")
+
+    #Get user's input
+        choice=input("Please enter attack number:")
+
+    #  Use try except to ensure the user has input a number; if they didn't tell them so and then use 'continue' to restart the loop
+        try:
+            choice=int(choice)-1
+            if choice<0 or choice >= len(user_attacks):
+                continue
+        except:
+            print("Please enter a number:")
+            continue
+
+    #Apply damage
+        damage=user_attacks[choice]["damage"]
+        current_health-=damage
+
+        print(f"You used {user_attacks[choice]["move"]}")
+
+    #Show remaining health
+        if current_health>0:
+            print(f"{enemy["name"]} has {current_health} health points left ")
+        else:
+            print(f"{enemy["name"]} has 0 health points left")
+            print(f"\n Yayyy. You defeated the wild {enemy["name"]}. Good Job! ")
+
+
+# Run main code
+
+def main():
+    print("Main Menu!")
+    print("1. Wordle")
+    print("2. Magic 8-ball")
+    print("3.Pokemon battle")
+
+    choice = input("Please choose 1, 2, or 3: ")
+
+    if choice == "1":
+        run_wordle()
+
+    elif choice == "2":
+        run_magic_8ball()
+    
+    elif choice == "3":
+        run_pokemon()
+
+    else:
+        print("Invalid choice. Restart and choose 1, 2, or 3.")
+
+    
+
+#===============================
+# EXECUTION
+#===============================
+# Execute main code
+main()
+
+
+
+
+#===============================
+#===============================
+# EXTENSION
+# TODO Go back to each program you chose and structure them with functions. 
+# TODO Then recopy them over as multiple functions (rather than one)
+# NOTE The main() function in your programs can be renamed as run_program_name() so it doesn't clash with this program's main()
